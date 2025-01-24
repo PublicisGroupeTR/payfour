1e8855de1b4c5a463c304eeb69f7e0ddbf161e47
MahmutBilalTekiroglu <163127035+MahmutBilalTekiroglu@users.noreply.github.com>
2025-01-24 18:35:49 +0300

    commit

---
+++ ios/EnQualify/Pages/BottomSheet/CustomBottomSheetViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CustomBottomSheetViewController.swift
+//  Payfour
+//
+//  Created by Mahmut Bilal Tekiroğlu on 24.01.2025.
+//
+
+import UIKit
+
+class CustomBottomSheetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
